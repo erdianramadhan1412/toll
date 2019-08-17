80303ccf9476e296aed54fd4ea055468f4b451a5
erdianramadhan1412 <53484354+erdianramadhan1412@users.noreply.github.com>
2019-08-17 20:14:19 +0700

    Add files via upload

---
+++ anakcilik.sh
@@ -0,0 +1,127 @@
+#!/system/bin/bash
+
+clear
+
+figlet "WELCOME TO" | lolcat
+sleep 2 
+figlet "TOOLS" | lolcat
+sleep 1
+toilet -f mono9 -F gay "Rama"
+
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+
+sleep 1
+
+echo "\033[34;1mAuthor By Rama"
+echo '\033[31;1mTUNGGU SEBENTAR'
+sleep 3
+echo '\033[33;1mOke'
+sleep 1
+
+clear
+
+
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "\033[36;1m"
+echo $green "1.> [spam Telepon]"
+echo "============================" | lolcat
+echo $yellow "2.> [dark fb]"
+echo "============================" | lolcat
+echo $green "3.> [autombf hack fb]"
+echo "============================" | lolcat
+echo $red "4.> [chat pacar bot]"
+echo "============================" | lolcat
+echo $green "5.> [spamsms]"
+echo "============================" | lolcat
+echo $blue "6.> [Jadwal sholat]"
+echo "============================" | lolcat
+echo $yellow "0.> exit"
+echo "============================" | lolcat
+read -p "PILIH NO.#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "FAUZAN" | lolcat
+git clone https://github.com/siputra12/prank
+mv prank $HOME
+cd $HOME/prank
+php call.php
+prank
+fi
+
+if
+[ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+figlet "FAUZAN" | lolcat
+git clone https://github.com/pashayogi/SETAN
+mv SETAN $HOME
+cd $HOME/SETAN
+python2 SETAN.py
+SETAN
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+figlet "FAUZAN" | lolcat
+git clone https://github.com/V4N654T/autombf
+mv autombf $HOME
+cd $HOME/autombf
+python2 autombf.py
+autombf
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+git clone https://gitlab.com/Sumarr-ID/Bot
+mv Bot $HOME
+cd $HOME/Bot
+bash bot.sh
+Bot
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+figlet "fauzan" | lolcat
+git clone https://github.com/4L13199/LITESPAM
+mv LITESPAM $HOME
+cd $HOME/LITESPAM
+sh LITESPAM.sh
+fi
+
+if
+[ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+figlet "OKE" | lolcat
+git clone https://github.com/karjok/sholat/
+mv sholat $HOME
+cd $HOME/sholat
+python sholat.py
+sholat
+fi
+
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+echo "terimakasih sudah menggunakan toll ini"
+sleep 1
+echo "author by Rama"
+sleep 1
+exit
+fi
+
+if [ $bro = ] || [ $bro = ]
+then
+echo "silahkan pilih"
+sh anakcilik.sh
